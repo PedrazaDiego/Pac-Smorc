1f5138af7209839d5ed734b1cfb37ae441dd7b52
PedrazaDiego <skapet01@gmail.com>
2021-11-19 20:25:38 -0500

    added point textures

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const body = document.querySelector('#section2')
 const plates = document.querySelectorAll('.plates')
 const grid = document.querySelector('.playground')
+const points = document.querySelectorAll('.meat')
 const orc = `<img src="https://i.imgur.com/Q9SaLOW.png" height="40px" alt="">`
 const texture = `<img src="https://i.imgur.com/LZeOFm4.png" height="40px" alt="">`
+const meat = `<img src="https://i.imgur.com/WETrLO9.png" height="20px" alt="">`
 const addPlate = document.createElement('div')
 const wall = document.querySelectorAll('.wall')
 const plateArray = []
@@ -11,39 +14,39 @@ const walls = [
     138, 142, 146, 150, 151, 152, 157, 159, 160, 161, 162, 163, 165, 171, 172, 174, 182, 184, 185, 188, 191, 193,
     194, 195, 196, 197, 199, 202, 205, 207, 208, 212, 216, 217, 219, 222, 227, 229, 231, 236, 239, 241, 244, 248,
     251, 253, 256, 258, 259, 261, 262, 263, 264, 265, 267, 268, 270]
-
-
-
-for (let i = 0; i < walls.length; i++){
-    plates[walls[i]].setAttribute('id', `${walls[i]}`)
-    // plates[walls[i]].classList.add('wall')
-    plates[walls[i]].classList.remove('plates')
-    plates[walls[i]].innerHTML = texture
-}// _____________________________________________________________ dotted walls
-
-plates.forEach( (n, n2) =>{
-    plateArray[n2] = n2
-})// _____________________________________________________________ set plateArray values
-
-
-const road = plateArray.filter( (n, n2) => {
-    // console.log(!walls[n])
-    return !walls.includes(n2)
-})// _____________________________________________________________ returns the difference of plateArray and Walls
-
-for (let i = 0; i < road.length; i++){
-    plates[road[i]].innerHTML = orc
-}// ______________________________________________________________ puts the player in the road
-
-
-
-
-
+const road = []
+let startingPoint = plates[0].innerHTML = orc
+let currentPoint = null
 
 
 
 //___________function___________//
 
+const gameBoard = () =>{
+    for (let i = 0; i < walls.length; i++){
+        plates[walls[i]].setAttribute('id', `${walls[i]}`)
+        plates[walls[i]].classList.remove('plates')
+        plates[walls[i]].innerHTML = texture
+    }//dotted walls
+    
+    plates.forEach( (n, n2) =>{
+        plateArray[n2] = n2
+    })//set plateArray values
+    
+    const x = plateArray.filter( (n, n2) => {
+        return !walls.includes(n2)
+        
+    })//returns the difference of plateArray and Walls
+    
+    for (let i = 1; i < x.length; i++){
+        // plates[x[0]].innerHTML = orc
+        plates[x[i]].classList.add(`meat`)
+        plates[x[i]].innerHTML = meat
+        road.push(x[i])
+    }//puts the player in the road
+    
+}
+gameBoard() //Sets gameboard
 
 
 
@@ -51,7 +54,12 @@ for (let i = 0; i < road.length; i++){
 
 
 
-
+// body.addEventListener('keydown', e => {
+//     console.log(e)
+// })
+// body.addEventListener('click', (e) =>{
+//     console.log(e.target)
+// })
 
 //___________events_____________//
 
@@ -72,4 +80,5 @@ for (let i = 0; i < road.length; i++){
 // console.log(plates)
 // console.log(walls)
 //console.log(road)
-console.log(plateArray.length)
+// console.log(plateArray.length)
+// console.log(road)
