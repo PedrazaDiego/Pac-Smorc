434b8e7d51a9929cc0d91ec109a1f1975b31164b
PedrazaDiego <skapet01@gmail.com>
2021-11-21 12:16:40 -0500

    enemy player moves one square

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const grid = document.querySelector('.playground')
 const points = document.querySelectorAll('.meat')
 const strength = document.querySelector('.score')
 const orc = `<img src="https://i.imgur.com/Q9SaLOW.png" height="40px" alt="">`
+const human = `<img src="https://i.imgur.com/68985hw.png" height="40px" alt="">`
 const texture = `<img src="https://i.imgur.com/bT2HoxX.png" height="40px" alt="">`
 const meat = `<img src="https://i.imgur.com/WETrLO9.png" height="20px" alt="">`
 const addPlate = document.createElement('div')
@@ -11,34 +12,39 @@ const wall = document.querySelectorAll('.wall')
 const plateArray = []
 const walls = [
     18, 20, 21, 23, 24, 25, 26, 27, 29, 30, 32, 35, 37, 40, 44, 46, 49, 52, 57, 59, 61, 66, 69, 70, 72, 76, 80,
-    82, 83, 89, 90, 91, 93, 95, 96, 97, 104, 105, 106, 114, 115, 116, 123, 125, 126, 127, 128, 129, 131, 136, 137,
+    82, 83, 89, 90, 91, 93, 95, 96, 97, 104, 105, 106, 114, 115, 116, 123, 125, 126, 128, 129, 131, 136, 137,
     138, 142, 146, 150, 151, 152, 157, 159, 160, 161, 162, 163, 165, 171, 172, 174, 182, 184, 185, 188, 191, 193,
     194, 195, 196, 197, 199, 202, 205, 207, 208, 212, 216, 217, 219, 222, 227, 229, 231, 236, 239, 241, 244, 248,
     251, 253, 256, 258, 259, 261, 262, 263, 264, 265, 267, 268, 270]
 const road = []
 let score = 0
 let orcStart = 0 
+let humanStart = 144
 let gameOn = false
+let rng = Math.floor(Math.random()*3.9) 
+console.log(rng)
+
 
 
 //___________function___________//
 
 
-for (let i = 0; i < 288; i += 17){
-    plates[i].classList.add('border-left')
+
+const setBorders = () => {
+    for (let i = 0; i < 288; i += 17){
+        plates[i].classList.add('border-left')
+        }
+    for (let i = 0; i < 17; i++){
+        plates[i].classList.add('border-up')
     }
-for (let i = 0; i < 17; i++){
-    plates[i].classList.add('border-up')
+    for (let i = 288; i > 0; i -= 17){
+        plates[i].classList.add('border-right')
+    }
+    for (let i = 288; i > 271; i--){
+        plates[i].classList.add('border-down')
+    }
 }
-for (let i = 288; i > 0; i -= 17){
-    plates[i].classList.add('border-right')
-}
-for (let i = 288; i > 271; i--){
-    plates[i].classList.add('border-down')
-}
-
-
-
+setBorders()
 
 
 const gameBoard = () =>{  //__________________________________dotted walls
@@ -57,13 +63,72 @@ const gameBoard = () =>{  //__________________________________dotted walls
         
     })
     
-    for (let i = 1; i < x.length; i++){  //puts the player in the road
+    for (let i = 1; i < x.length; i++){  //puts the meat in the road
         plates[x[i]].classList.add(`meat`)
         plates[x[i]].innerHTML = meat
         road.push(x[i])
     }
 }
 gameBoard() //Sets gameboard
+
+const humanMove = () => {
+    if(!gameOn){
+        if(rng === 0){
+            if(plates[humanStart].classList.contains('border-left')){
+                return false
+            }
+            if(plates[humanStart - 1].classList.contains('0')){
+                return false
+            }
+            humanStart -= 1
+            plates[humanStart].classList.replace('meat', 'human')
+            plates[humanStart].innerHTML = human
+            plates[humanStart + 1].innerHTML = meat
+            plates[humanStart + 1].classList.remove('human')
+        }
+        if(rng === 1){
+            if(plates[humanStart].classList.contains('border-up')){
+                return false
+            }
+            if(plates[humanStart - 17].classList.contains('0')){
+                return false
+            }
+            humanStart -= 17
+            plates[humanStart].classList.replace('meat', 'human')
+            plates[humanStart].innerHTML = human
+            plates[humanStart + 17].innerHTML = meat
+            plates[humanStart + 17].classList.remove('human')
+        }
+        if(rng === 2){
+            if(plates[humanStart].classList.contains('border-right')){
+                return false
+            }
+            if(plates[humanStart + 1].classList.contains('0')){
+                return false
+            }
+            humanStart += 1
+            plates[humanStart].classList.replace('meat', 'human')
+            plates[humanStart].innerHTML = human
+            plates[humanStart - 1].innerHTML = meat
+            plates[humanStart - 1].classList.remove('human')
+        }
+        if(rng === 3){
+            if(plates[humanStart].classList.contains('border-down')){
+                return false
+            }
+            if(plates[humanStart + 17].classList.contains('0')){
+                return false
+            }
+            humanStart += 17
+            plates[humanStart].classList.replace('meat', 'human')
+            plates[humanStart].innerHTML = human
+            plates[humanStart - 17].innerHTML = meat
+            plates[humanStart - 17].classList.remove('human')
+        }
+    }
+}
+humanMove()
+
 
 const playerMove = () =>{
         document.addEventListener('keydown', function(e) {
@@ -140,6 +205,7 @@ const playerMove = () =>{
 }//block taken from https://www.codegrepper.com/code-examples/javascript/addeventlistener+arrow+keys
 playerMove()//Sets player movement
 
+
 const scoreUpdate = () => {
     if(plates[orcStart].classList.contains('meat')){
         plates[orcStart].classList.remove('meat')
@@ -149,13 +215,13 @@ const scoreUpdate = () => {
     }
 }
 
+
 const won = () => {
-    if(score === 1770){
+    if(score === 1810){
         gameOn = true
     }
 }
 //___________events_____________//
-
 
 
 
